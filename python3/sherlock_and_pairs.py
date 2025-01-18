17d5cdcff7b6352f819c34710c76ed13b6a3c926
Oli <ofcunnington@hotmail.com>
2025-01-18 11:42:03 +0100

    challenge completed

--- python3/sherlock_and_pairs.py
+++ python3/sherlock_and_pairs.py
@@ -15,21 +15,21 @@ import sys
 # i j n     -> ij, ji, in, ni, jn, nj                           = 6  -> count * count - 1
 # i j n k   -> ij, ji, in, ni, ik, ki, jn, nj, jk, kj, nk, kn   = 12 -> count * count - 1
 
+
+### goddamn builtin .count() was F*cking me on big lists..
+
 def solve(a):
     # Write your code here
     c_dict = {}
-    summation = 0
     for x in a:
         if x not in c_dict.keys():
-            cou = a.count(x)
-            if cou > 1:
-                c_dict[x] = 0
-                summation += cou * (cou-1)
+            c_dict[x] = 0
+        c_dict[x] += 1
     # total = 0
     # for x in c_dict.keys():
     #     total += c_dict[x] * (c_dict[x] - 1)
     # return total
-    return summation
+    return sum([x * (x - 1) for x in c_dict.values()])
 
 
 if __name__ == '__main__':
