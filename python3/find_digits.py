4f6ac02846d39c87fd6f6d1294465a349ab8500f
Oli <ofcunnington@hotmail.com>
2025-01-31 05:51:19 +0100

    daily challenge

---
+++ python3/find_digits.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'findDigits' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER n as parameter.
+#
+
+def findDigits(n):
+    lst = str(n)
+    total = 0
+    for x in lst:
+        total += 1 if (int(x) != 0 and n % int(x) == 0) else 0
+    return total
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        result = findDigits(n)
+
+        fptr.write(str(result) + '\n')
+
+    fptr.close()
