283d60cc649fe11911d7b16192528b2b3ed915cd
Oli <ofcunnington@hotmail.com>
2025-02-05 13:51:29 +0100

    daily challenge

---
+++ python3/cut_the_sticks.py
@@ -0,0 +1,38 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'cutTheSticks' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def cutTheSticks(arr):
+    # Write your code here
+    res = []
+    while(len(arr) > 0):
+        res.append(len(arr))
+        smallest = min(arr)
+        arr = [x - smallest for x in arr if x-smallest > 0]
+        
+    return res
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    result = cutTheSticks(arr)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
