e1f38db7ae428d9a823abe78b215490708c71dcf
Oli <ofcunnington@hotmail.com>
2025-02-09 12:43:31 +0100

    daily challenge - incomplete

---
+++ python3/acm_icpc_team.py
@@ -0,0 +1,47 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'acmTeam' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts STRING_ARRAY topic as parameter.
+#
+
+def acmTeam(topic):
+    # Write your code here
+    pass
+    # either
+    #   -merge lists with OR
+    #   -count(1)
+    #   -return count(max(counts))
+    # or
+    #   - list comp lists with either == 1
+    #   - return count(max(len(comped-lists)))
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    topic = []
+
+    for _ in range(n):
+        topic_item = input()
+        topic.append(topic_item)
+
+    result = acmTeam(topic)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
