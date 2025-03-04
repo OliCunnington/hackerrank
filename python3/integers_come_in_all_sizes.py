6f281fa2dc04b7fd7489d520c0c33d8f07dff39b
Oli <ofcunnington@hotmail.com>
2025-03-04 05:55:29 +0100

    daily challenge

---
+++ python3/integers_come_in_all_sizes.py
@@ -0,0 +1,6 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+ints = []
+for i in range(4):
+    ints.append(int(input()))
+
+print(ints[0] ** ints [1] + ints[2] ** ints[3])
