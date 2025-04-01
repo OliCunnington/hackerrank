961f69103ab0e8c7f9051f7df86481841d9a4ec3
Oli <ofcunnington@hotmail.com>
2025-04-01 10:52:29 +0200

    daily challenge

---
+++ python3/mp_min_and_max.py
@@ -0,0 +1,8 @@
+import numpy
+
+n_m = list(map(int, input().split()))
+arr = numpy.empty((n_m[0], n_m[1]))
+for i in range(n_m[0]):
+    arr[i] = list(map(int, input().split()))
+
+print(int(numpy.max(numpy.min(arr, axis=1))))
