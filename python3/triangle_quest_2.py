bfa46f91ac6eb088eacb8c91be491e08702d2134
Oli <ofcunnington@hotmail.com>
2025-04-29 08:22:21 +0200

    daily challenge

---
+++ python3/triangle_quest_2.py
@@ -0,0 +1,5 @@
+### evil "challenge"
+### stoopid maths
+
+for i in range(1,int(input())+1): #More than 2 lines will result in 0 score. Do not leave a blank line also
+    print(((10**i - 1)//9)**2) #*list(range(1, i+1)), *list(range(1, i+1))[-2::-1], sep="")
