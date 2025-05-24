6d6013c5932b91db146e058b319d106b5eb6a853
Oli <ofcunnington@hotmail.com>
2025-05-24 07:15:29 +0200

    daily challenge

---
+++ python3/marcs_cakewalk.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'marcsCakewalk' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts INTEGER_ARRAY calorie as parameter.
+#
+
+def marcsCakewalk(calorie):
+    sum = 0
+    calorie.sort(reverse=True)
+    for i in range(len(calorie)):
+        sum += pow(2, i) * calorie[i]
+    return sum
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    calorie = list(map(int, input().rstrip().split()))
+
+    result = marcsCakewalk(calorie)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
