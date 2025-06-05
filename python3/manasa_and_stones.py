2a6d04503d3720d828a5145fe63e2365b9c06191
Oli <ofcunnington@hotmail.com>
2025-06-05 12:15:15 +0200

    daily challenge - incomplete

---
+++ python3/manasa_and_stones.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'stones' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER a
+#  3. INTEGER b
+#
+
+def stones(n, a, b):
+    _as = [a for a in range(n-1)]
+    _bs = [b for b in range(n-1)] # -1 since we dont care about the starting 0...
+    
+    ### what the fuck is the thing i need here? not permutations... not dot product
+    ### cross product?!
+    ### _as X _bs... ?
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        n = int(input().strip())
+
+        a = int(input().strip())
+
+        b = int(input().strip())
+
+        result = stones(n, a, b)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
