7a2151422faebba3ae4046ca127425babfc02749
Oli <ofcunnington@hotmail.com>
2025-06-25 07:37:34 +0200

    daily challenge - incomplete

--- python3/beautiful_triplets.py
+++ python3/beautiful_triplets.py
@@ -19,34 +19,42 @@ def testBeat(d, arr):
     return arr[0] < arr[1] < arr[2] and arr[1] - arr[0] == d and arr[2] - arr[1] == d
 
 def beautifulTriplets(d, arr):
+    # Write your code here
+    ## make triplets, map test across
+    ## ascending list, < not <=
+    # u_arr = set(arr)
+    # print(u_arr)
+    ## need to make trips with just later numbers...
+    # for i in range(len(arr)):
+    #   trips_from(arr[i], arr[i+1::]) ?
+    ## zip()
+    # return len([1 for x in itertools.combinations(arr, 3) if testBeat(d, x)])
+    ## time out on large lists
+    ## need to only take elements later in list that != element...
+    ## can i do with set and multiply by... * count if count > 1 (??) 
+    # print(trips)
+    # b_trips = [testBeat(d, t) for t in trips]
+    # return b_trips.count(True)
     counts = {}
     for i in arr:
         if i in counts.keys():
             counts[i] += 1
         else:
             counts[i] = 1
-    # Write your code here
-    # make triplets, map test across
-    # ascending list, < not <=
-    # u_arr = set(arr)
-    # print(u_arr)
-    # need to make trips with just later numbers...
-    # for i in range(len(arr)):
-    # trips_from(arr[i], arr[i+1::]) ?
-    # zip()
-    # return len([1 for x in itertools.combinations(arr, 3) if testBeat(d, x)])
-    # time out on large lists
-    # need to only take elements later in list that != element...
-    # can i do with set and multiply by... * count if count > 1 (??) 
     trips = list(itertools.combinations(set(arr), 3))
-    # print(trips)
-    # b_trips = [testBeat(d, t) for t in trips]
     summat = 0
     for t in trips:
         if testBeat(d, t):
             summat += 1 * counts[t[0]] * counts[t[1]] * counts[t[2]]
-    # return b_trips.count(True)
     return summat
+    
+    ## need to do just trips from after?
+    # for i in range(len(arr)-2):
+    #   trips_from(arr[i], doubles_from(arr[i+1], arr[i+2:]))
+    # trips_from(n, arr):
+    #   return [[n, a[0], a[1]] for a in arr]
+    # doubles_from(n, arr):
+    #   return [[n, i] for i in arr]
     
 
 if __name__ == '__main__':
