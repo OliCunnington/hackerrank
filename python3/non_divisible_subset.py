620a7c75ccd1ad22ce01fca3d88175c0ef5bc8fc
Oli <ofcunnington@hotmail.com>
2025-07-16 19:09:18 +0200

    daily challenge - correct but timeout

--- python3/non_divisible_subset.py
+++ python3/non_divisible_subset.py
@@ -6,6 +6,10 @@ import random
 import re
 import sys
 
+
+### INCOMPLETE - timeout on large list
+
+
 #
 # Complete the 'nonDivisibleSubset' function below.
 #
@@ -18,22 +22,7 @@ import sys
 def nonDivisibleSubset(k, s):
     s = list(set(s))
     
-    # s_c = s.copy()
     s_s = []
-    
-    # for j in range(len(s_c) - 1):
-    #     for i in s_c[j+1:]:
-    #         if (s_c[j] + i) % k == 0:
-    #             s_c.remove(i)
-    #     s_s.append(len(s_c))
-    #     s_c = s.copy()
-    
-    # Kekw, i need to check each other element in list against others...
-    
-    # how the fuck does for x in as loop interact with as.remove(x) ?!
-    
-    # i need to construct the lists, not remove from copy
-    # this will allow me to add an element if NONE (a+b) % k == 0
     
     for i in range(len(s)):
         s_s.append([s[i]])
