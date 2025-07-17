12ee4acac5f43d19b44066ce5be32d517148e07f
Oli <ofcunnington@hotmail.com>
2025-07-17 07:31:24 +0200

    daily challenge - correct but timeout

---
+++ python3/happy_ladybugs.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+### INCOMPLETE - passing 9/12 testcases
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'happyLadybugs' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING b as parameter.
+#
+
+def happyLadybugs(b):
+    # get letter counts
+    counts = {}
+    has_space = False
+    for i in b:
+        if i == "_":
+            has_space = True
+        elif i in counts.keys():
+            counts[i] += 1
+        else:
+            counts[i] = 1
+    
+    if 1 in counts.values() or not has_space:
+        # print("NO")
+        return "NO"
+    # print("YES")    
+    return "YES"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    g = int(input().strip())
+
+    for g_itr in range(g):
+        n = int(input().strip())
+
+        b = input()
+
+        result = happyLadybugs(b)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
